e2c0a2433ee6f3e9bb690cee1e2b73c570d1bc01
Patchi92 <lechieu@hotmail.com>
2016-03-12 14:12:09 +0100

    Setting up the scene

---
+++ Voice Game Mark 1/Assets/Narrator.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Narrator : MonoBehaviour {
+
+	AudioSource player_Dialog;
+	public AudioClip TestSound;
+
+	// Use this for initialization
+	void Start () {
+	
+		player_Dialog = GetComponent<AudioSource>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+	}
+
+	public void Test () {
+		Debug.Log("lol2");
+		player_Dialog.clip = TestSound;
+		player_Dialog.Play();
+	}
+
+	public void DayOne () {
+
+	}
+
+	public void DayTwo () {
+
+	}
+
+	public void DayThree () {
+
+	}
+
+	public void DayFour () {
+
+	}
+}
---
+++ Voice Game Mark 1/Assets/Trigger.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trigger : MonoBehaviour {
+
+	public GameObject narrator;
+
+	public bool test;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.tag == "Player") {
+			if(test) {
+			Debug.Log("lol");
+			narrator.GetComponent<Narrator>().Test();
+			Destroy(gameObject);
+			}
+		}
+	}
+}

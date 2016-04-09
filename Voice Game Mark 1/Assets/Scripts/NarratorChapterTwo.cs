0d022ce7c25581dcd66cad91f6b9feb5ab214ef6
Patchi92 <lechieu@hotmail.com>
2016-04-09 14:54:42 +0200

    Working on sounds for chapter one

---
+++ Voice Game Mark 1/Assets/Scripts/NarratorChapterTwo.cs
@@ -0,0 +1,180 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NarratorChapterTwo : MonoBehaviour {
+
+	public GameObject player;
+
+	public GameObject bedDoor;
+	public GameObject kitchenDoor;
+	public GameObject hallDoor;
+	public GameObject houseDoor;
+	public GameObject LighthouseDoor;
+
+	public GameObject soundEffect;
+
+	AudioSource player_Dialog;
+	AudioSource sound_Effect;
+
+	// UI
+
+	public GameObject fade;
+	public GameObject speaking;
+	public GameObject interactText;
+	public GameObject infoText;
+
+
+	//Sound Files
+
+	public AudioClip intro;
+	public AudioClip helloKitchen;
+	public AudioClip changeInFrame;
+	public AudioClip unusualLight;
+	public AudioClip answerComePartOne;
+	public AudioClip answerComePartTwo;
+	public AudioClip answerComeAgainPartOne;
+	public AudioClip answerComeAgainPartTwo;
+	public AudioClip whoAreYou;
+	public AudioClip aknowledgeYourself;
+	public AudioClip rememberName;
+	public AudioClip realiseName;
+
+	// Door
+
+	public bool unlockLightDoorOne = false;
+
+	// Use this for initialization
+	void Start () {
+	
+		player_Dialog = GetComponent<AudioSource>();
+		sound_Effect = soundEffect.GetComponent<AudioSource>();
+
+		bedDoor.GetComponent<OpenDoor>().DoorFunction();
+		kitchenDoor.GetComponent<OpenDoor>().DoorFunction();
+		houseDoor.GetComponent<OpenDoor>().DoorFunction();
+		LighthouseDoor.GetComponent<OpenDoor>().DoorFunction();
+
+		player.GetComponent<MovementScript>().playerLockdown = false;
+
+		fade.SetActive(true);
+		fade.GetComponent<FadeBlack>().FadeOut();
+		player.GetComponent<MovementScript>().StunPlayer(4);
+		Invoke("Begin",2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(player_Dialog.isPlaying) {
+			speaking.SetActive(true);
+		}
+
+		if(!player_Dialog.isPlaying) {
+			speaking.SetActive(false);
+		}
+
+
+	}
+
+	public void TurnTextOn() {
+		interactText.SetActive(true);
+		infoText.SetActive(true);
+
+		if(gameObject.GetComponent<AudioSource>().isPlaying) {
+			interactText.GetComponent<Text>().text = "Waiting for you to stop talking";
+		} else {
+			interactText.GetComponent<Text>().text = "Press 'E' to interact";
+		}
+	}
+
+	public void TurnTextOff() {
+		interactText.SetActive(false);
+		infoText.SetActive(false);
+	}
+
+
+	public void Begin() {
+		player_Dialog.clip = intro;
+		player_Dialog.Play();
+	}
+
+	public void KitchenHello() {
+		player_Dialog.clip = helloKitchen;
+		player_Dialog.Play();
+	}
+
+	public void StrangeFrame() {
+		player_Dialog.clip = changeInFrame;
+		player_Dialog.Play();
+
+		Invoke("OpenDoorToLight",6);
+	}
+
+	public void OpenDoorToLight() {
+		hallDoor.GetComponent<OpenDoor>().DoorFunction();
+	}
+
+	public void UnusualLight() {
+		player_Dialog.clip = unusualLight;
+		player_Dialog.Play();
+	}
+
+	public void LighthouseEnterOne() {
+		player_Dialog.clip = answerComePartOne;
+		player_Dialog.Play();
+	}
+
+	public void LighthouseEnterTwo() {
+		player_Dialog.clip = answerComePartTwo;
+		player_Dialog.Play();
+
+		player.GetComponent<MovementScript>().speed = 5;
+	}
+
+	public void LighthouseEnterThree() {
+		player_Dialog.clip = answerComeAgainPartOne;
+		player_Dialog.Play();
+	}
+
+	public void LighthouseEnterFour() {
+		player_Dialog.clip = answerComeAgainPartTwo;
+		player_Dialog.Play();
+	}
+
+	public void LighthouseTopQuestionOne() {
+		player_Dialog.clip = whoAreYou;
+		player_Dialog.Play();
+
+		player.GetComponent<MovementScript>().speed = 2;
+	}
+
+	public void LighthouseTopQuestionTwo() {
+		player_Dialog.clip = aknowledgeYourself;
+		player_Dialog.Play();
+	}
+
+	public void LighthouseTopQuestionThree() {
+		player_Dialog.clip = rememberName;
+		player_Dialog.Play();
+	}
+
+	public void LighthouseTopQuestionFour() {
+		player_Dialog.clip = realiseName;
+		player_Dialog.Play();
+
+		Invoke("Fade",2);
+		Invoke("NextLevel",8);
+	}
+
+
+
+	void Fade() {
+		fade.SetActive(true);
+		fade.GetComponent<FadeBlack>().FadeIn();
+	}
+
+	void NextLevel() {
+		//Application.LoadLevel(2);
+	}
+}
---
+++ Voice Game Mark 1/Assets/Scripts/Sleep.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sleep : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay(Collider other) {
+		if(other.gameObject.tag == "Player") {
+
+			other.gameObject.transform.localScale = new Vector3(1, 0.1f, 1);
+			other.gameObject.GetComponent<MovementScript>().speed = 1.5f;
+
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if(other.gameObject.tag == "Player") {
+
+			other.gameObject.transform.localScale = new Vector3(1, 1, 1);
+			other.gameObject.GetComponent<MovementScript>().speed = 2;
+
+		}
+	}
+}
